265e7ff6f00fd05c072023db3e3d4f6ec9903626
palat.itpro@gmail.com <palat-itpro@gmail.com>
2021-04-21 23:59:06 +1000

    Shipment data display format

--- wmsv3/src/app/import/shipmenttable/shipmenttable.component.ts
+++ wmsv3/src/app/import/shipmenttable/shipmenttable.component.ts
@@ -1,30 +1,53 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTable } from '@angular/material/table';
-import { ShipmenttableDataSource, ShipmenttableItem } from './shipmenttable-datasource';
+import {
+  ShipmenttableDataSource,
+  ShipmenttableItem,
+} from './shipmenttable-datasource';
+import { MatTableDataSource } from '@angular/material/table';
+// import { shipmentData as shipmentDatamModel } from './demo';
 
 @Component({
   selector: 'app-shipmenttable',
   templateUrl: './shipmenttable.component.html',
-  styleUrls: ['./shipmenttable.component.css']
+  styleUrls: ['./shipmenttable.component.css'],
 })
-export class ShipmenttableComponent implements AfterViewInit {
+export class ShipmenttableComponent implements AfterViewInit, OnInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
   @ViewChild(MatTable) table!: MatTable<ShipmenttableItem>;
-  dataSource: ShipmenttableDataSource;
+  dataSource: any;
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-  displayedColumns = ['id', 'name'];
+  displayedColumns = ['SHIPMENT', 'CONT'];
+  subTableColumns = ['SKU', 'QTY', 'TON'];
 
-  constructor() {
-    this.dataSource = new ShipmenttableDataSource();
+  constructor(private afs: AngularFirestore) {
+    this.dataSource = new MatTableDataSource();
+  }
+
+  totalCont(i: number) {
+    return this.dataSource[i].containerList.length;
   }
 
   ngAfterViewInit(): void {
     this.dataSource.sort = this.sort;
     this.dataSource.paginator = this.paginator;
-    this.table.dataSource = this.dataSource;
+    this.afs
+      .collection('lae_shipment')
+      .valueChanges()
+      .subscribe((res: any) => {
+        this.dataSource = res;
+      });
+  }
+  // data = shipmentDatamModel;
+  ngOnInit() {
+    // this.afs
+    //   .collection('lae_shipment')
+    //   .doc(this.data.shipmentNumber)
+    //   .set(this.data);
   }
 }
--- wmsv3/src/app/layout/sidenav/sidenav.component.ts
+++ wmsv3/src/app/layout/sidenav/sidenav.component.ts
@@ -3,6 +3,14 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 
+export interface NavItem {
+  displayName: string;
+  disabled?: boolean;
+  iconName: string;
+  route?: string;
+  children?: NavItem[];
+}
+
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
@@ -15,6 +23,36 @@ export class SidenavComponent {
       map((result) => result.matches),
       shareReplay()
     );
+
+  menu: NavItem[] = [
+    {
+      displayName: 'Import',
+      iconName: 'description',
+      children: [
+        {
+          displayName: 'Shipment',
+          iconName: 'how_to_reg',
+          route: 'shipment',
+        },
+        {
+          displayName: 'Todos',
+          iconName: 'waves',
+          route: '/todos',
+        },
+      ],
+    },
+    {
+      displayName: 'Perfiles',
+      iconName: 'group',
+      children: [
+        {
+          displayName: 'Búsqueda Perfil',
+          iconName: 'search',
+          route: '/busquedaperfiles',
+        },
+      ],
+    },
+  ];
 
   panelOpenState = false;
 
